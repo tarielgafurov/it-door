cba3621ec9c6e2420f95fec545ad36919c10bd50
Daniel09_Ada <dadasarov@gmail.com>
2024-12-04 16:31:32 +0600

    input-finish

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -4,9 +4,9 @@ import TextField from '@mui/material/TextField';
 
 
 
-const Input = ({placeholder, type, onChange, onClick, key, value, id, label }, props ) => {
+const Input = ({placeholder, type, onChange, key, value, id, label }, props ) => {
   return (
-    <InputStyled type={type} placeholder={placeholder} onChange={onChange} onClick={onClick} key={key} value={value} id={id} label={label} style={{...props}} />
+    <InputStyled type={type} placeholder={placeholder} onChange={onChange} key={key} value={value} id={id} label={label} style={{...props}} />
     
   )
 }
@@ -16,4 +16,5 @@ export default Input;
 const InputStyled = styled(TextField)`
   width: ${(props) => props.width || "20.938rem" };
   top: ${(props) => props.Top || "20px"};
-`
+` 
+
