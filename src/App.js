86217a39fdb59ffcd94461e10e283a788402cce6
Baiysh <bajysmamatisakov5@gmail.com>
2024-12-09 01:30:51 -0800

    users-finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import UsersUs from './layout/Users';
 
 function App() {
   
   return (
     <div className="App">
-      <UsersUs/>
+    
     </div>
   );
 }
 
 export default App;
+
+
