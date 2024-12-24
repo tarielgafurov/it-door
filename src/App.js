bde30037dd05b5458c78ccb1189678788d21859d
Akjol <usonov_akzol05@gmail.com>
2024-12-24 16:33:41 +0600

    Educational

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css'
-import Main from './components/The EducationalProcess/Main';
 
 function App() {
 
   return (
     <div className="App">
-      <Main/>
+
     </div>
   );
 } 
