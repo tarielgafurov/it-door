bf3e0aee8c924dbc18e27852a5f42d664e1fc80d
Daniel09_Ada <dadasarov@gmail.com>
2025-01-03 17:44:51 +0600

    finish-InfoDiv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import DivContainer from './components/informationTraining/DivContainer';
 
 function App() {
   return (
     <div className="App">
-      <DivContainer/>
     </div>
   );
 }
